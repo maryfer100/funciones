aa23493e0e4d1ae8e481fbc1cce607edae1d6e5b
maryfer100 <64994655+maryfer100@users.noreply.github.com>
2023-02-20 20:59:08 -0600

    Add files via upload

---
+++ cnetros.sql
@@ -0,0 +1,11 @@
+﻿create table catalogocentros(
+	numerocentro integer,
+	nombrecentro varchar
+)
+
+select * from catalogocentros
+
+insert into catalogocentros (numerocentro, nombrecentro) values(230399,'SOLUCION DE OPERACIONES');
+insert into catalogocentros (numerocentro, nombrecentro) values(250595,'COMUNICACION INTERNA');
+insert into catalogocentros (numerocentro, nombrecentro) values(230390,'DESARROLLO COS');
+insert into catalogocentros (numerocentro, nombrecentro) values(250590,'DBA ATENCION');
